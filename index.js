a8124b1fd2896885d8708412373aeabaa7564fbb
MattV <mvpetersen1@gmail.com>
2021-02-22 18:31:37 -0700

    updated HTML const

--- index.js
+++ index.js
@@ -10,6 +10,102 @@ const Intern = require('./lib/Intern')
 //file name and location of output.
 const filePath = "../dist/";
 
+const managerHTML = answers =>
+`
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Engeneering Team</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="jumbotron jumbotron-fluid mb-0">
+        <div class="container">
+            <h1 class="display-3">Engineering Team</h1>
+            <hr class="my-2">
+        </div>
+    </div>
+    <div class="container-fluid bg-secondary mt-2">
+        <div id="cards" class="card-columns">
+            <div id="manager" class="card">
+                <div class="card-body bg-light">
+                    <div class="card-header bg-info">
+                        <h4 class="card-title">Name: ${managerAnswers.employeeName}</h4>
+                        <h5 class="card-text">Manager </h5>
+                    </div>
+                    <div>
+                        <p class="card-text">ID: ${managerAnswers.employeeId}</p>
+                        <p class="card-text">EMail: ${managerAnswers.employeeEmail}</p>
+                        <p class="card-text">Office Number: ${managerAnswers.managerOffice}</p>
+                    </div>
+                </div>
+            </div>
+`;
+
+const engineerHTML = (answers) =>
+`
+<div id="engineer" class="card">
+<div class="card-body bg-light">
+    <div class="card-header bg-info">
+        <h4 class="card-title">Name: ${engeneerAnswers.employeeName}</h4>
+        <h5 class="card-text">Engeneer </h5>
+    </div>
+    <div>
+        <p class="card-text">ID: ${engeneerAnswers.employeeId}</p>
+        <p class="card-text">EMail: ${engeneerAnswers.employeeEmail}</p>
+        <p class="card-text">GitHub: ${engeneerAnswers.engeneerGithub}</p>
+    </div>
+</div>
+</div>
+`
+
+const internHTML = (answers) =>
+`
+<div id="intern" class="card">
+<div class="card-body bg-light">
+    <div class="card-header bg-info">
+        <h4 class="card-title">Name:${internAnswers.employeeName}</h4>
+        <h5 class="card-text">Title Intern</h5>
+    </div>
+    <div>
+        <p class="card-text">ID: ${internAnswers.employeeId}</p>
+        <p class="card-text">EMail: ${internAnswers.employeeEmail}</p>
+        <p class="card-text">School: ${internAnswers.internSchool}</p>
+    </div>
+</div>
+</div>
+`
+
+const doneHTML = (answers) =>
+`
+</div>
+
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
+`
+
 //write the file.
 // const writeFileAsync = util.promisify(fs.writeFile);
 const employeeChoice = () => {
@@ -89,7 +185,7 @@ const managerPrompts = () => {
 
     ]).then(managerAnswers => {
         console.table(managerAnswers)
-        const employee = new Employee(managerAnswers.employeeName, managerAnswers.employeeId, managerAnswers.employeeEmail);
+        const employee = new Employee(managerAnswers.employeeName, managerAnswers.employeeId, managerAnswers.employeeEmail, managerAnswers.managerOffice);
         //append Manager card to HTML
 
         console.log(employee)
